96f95da33fc507b626d5ca6b811621d3911e0b79
CosmonautJones <travisj_jones@hotmail.com>
2018-01-12 13:32:06 -0600

    working on getting the breeds to show

--- dogBreedApp/App.js
+++ dogBreedApp/App.js
@@ -1,14 +1,53 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
 
 const axios = require('axios');
 
 export default class App extends React.Component {
+	state = {
+		breeds: []
+	};
+
+	componentDidMount() {
+		axios
+			.get('https://dog.ceo/api/breeds/list/all')
+			.then(response => {
+				this.setState(prevState => {
+					let { breeds } = prevState;
+					return {
+						breeds: response.data.message
+					};
+				});
+			})
+			.catch(error => {
+				console.log(error);
+			});
+	}
+
 	render() {
 		return (
-			<View style={styles.container}>
-				<Text>Here are a list of dog breeds</Text>
-			</View>
+			<ScrollView>
+        <View style ={styles.header}/>
+				<View style={styles.container}>
+					<Text>Here are a list of dog breeds</Text>
+					<FlatList
+						keyExtractor={dog => dog.message}
+						style={styles.list}
+						data={this.state.breeds}
+						renderItem={({ dog }) => {
+							return (
+								<View>
+									<View style={styles.listCont}>
+										<Text style={styles.textItem}>{dog.message}</Text>
+										/>
+									</View>
+									<View style={styles.hr} />
+								</View>
+							);
+						}}
+					/>
+				</View>
+			</ScrollView>
 		);
 	}
 }
@@ -19,5 +58,26 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center'
+  },
+  header: {
+    height: 15
+  },
+	hr: {
+		height: 1,
+		backgroundColor: '#d3d3d3'
+	},
+	textItem: {
+		paddingLeft: 10,
+		paddingRight: 10,
+		fontSize: 16
+	},
+	listCont: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+		width: 400
+	},
+	list: {
+		width: '100%'
 	}
 });
