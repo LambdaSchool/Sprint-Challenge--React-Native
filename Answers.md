67dbd55a68ec9d0191c375adb95161c4714f46ce
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-01-12 14:03:33 -0700

    successfully fetching, needs styling work

--- Answers.md
+++ Answers.md
@@ -1,6 +1,8 @@
 # Answers.md
 
 1. What makes React Native apps different from hybrid apps built with Cordova and the like?
+  Hybrid apps are developed using web technology to achieve one code base for multiple platforms. Natively developed apps use only native code available for the platform for development, one app per platform, one code base per platform. (React-native compiles to most avaliable languages.)
 1. In what ways is React Native different syntactically from React?
+  CSS vs JSON elements.
 1. What are some draw backs of writing a mobile app in React Native versus the other native languages?
 1. What are some of the strengths of writing a mobile app in React Native versus the other native languages?
--- dogBreeds/App.js
+++ dogBreeds/App.js
@@ -1,13 +1,32 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import axios from 'axios';
 
 export default class App extends React.Component {
+  state = {
+    breeds: [],
+  };
+  componentWillMount() {
+    axios
+      .get('https://dog.ceo/api/breeds/list/all')
+      .then((list) => {
+        if (list) {
+          this.setState((prev) => {
+            const breeds = Object.keys(list.data.message)
+              .map((breed, i) => ({key: i, breed}));
+            return { breeds };
+          });
+        }
+      });
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <FlatList
+          style={styles.flexList}
+          data={this.state.breeds}
+          renderItem={({item}) => <Text>{item.breed}</Text>}
+        />
       </View>
     );
   }
