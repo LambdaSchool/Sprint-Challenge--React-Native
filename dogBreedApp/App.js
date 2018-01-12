88b59bf8e42687602fd9d251d28dee183bd4c740
CosmonautJones <travisj_jones@hotmail.com>
2018-01-12 14:07:37 -0600

    BREEDS ARE SHOWING. YES

--- dogBreedApp/App.js
+++ dogBreedApp/App.js
@@ -4,19 +4,19 @@ import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
 const axios = require('axios');
 
 export default class App extends React.Component {
-	state = {
-		breeds: []
-	};
+	constructor(props) {
+		super(props);
+		this.state = {
+			breeds: {}
+		};
+	}
 
 	componentDidMount() {
 		axios
 			.get('https://dog.ceo/api/breeds/list/all')
 			.then(response => {
-				this.setState(prevState => {
-					let { breeds } = prevState;
-					return {
-						breeds: response.data.message
-					};
+				this.setState({
+					breeds: response.data.message
 				});
 			})
 			.catch(error => {
@@ -27,19 +27,21 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<ScrollView>
-        <View style ={styles.header}/>
+				<View style={styles.header} />
 				<View style={styles.container}>
 					<Text>Here are a list of dog breeds</Text>
+				<View style={styles.header} />
 					<FlatList
 						keyExtractor={dog => dog.message}
 						style={styles.list}
-						data={this.state.breeds}
-						renderItem={({ dog }) => {
+						data={Object.keys(this.state.breeds)}
+						renderItem={({ item }) => {
 							return (
 								<View>
 									<View style={styles.listCont}>
-										<Text style={styles.textItem}>{dog.message}</Text>
-										/>
+										<Text style={styles.textItem}>
+											-{item.toString().toUpperCase()}
+										</Text>
 									</View>
 									<View style={styles.hr} />
 								</View>
@@ -58,24 +60,26 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center'
-  },
-  header: {
-    height: 15
-  },
+	},
+	header: {
+		height: 15
+	},
 	hr: {
-		height: 1,
+		height: 5,
 		backgroundColor: '#d3d3d3'
 	},
 	textItem: {
+		alignItems: 'center',
 		paddingLeft: 10,
 		paddingRight: 10,
-		fontSize: 16
+		fontSize: 12
 	},
 	listCont: {
 		flexDirection: 'row',
 		alignItems: 'center',
 		justifyContent: 'space-between',
-		width: 400
+		width: 400,
+		height: 23
 	},
 	list: {
 		width: '100%'
