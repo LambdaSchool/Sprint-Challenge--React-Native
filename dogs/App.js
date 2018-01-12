f879d8b3d718e56208763a7d7a58318fa90d5521
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-01-12 15:50:25 -0800

    YEAAAAAAH!

--- dogs/App.js
+++ dogs/App.js
@@ -1,39 +1,40 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import axios from 'axios';
 
 export default class Home extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {},
+    this.state = {
+      breeds: [{key: 1, breed: "test"}, {key: 2, breed: "test2"}],
     };
-  };
+  }
 
-//not sure why this isn't working but I'm out of ideas and time's up.
-
-  componentDidMount() {
+  componentWillMount() {
     axios.get('https://dog.ceo/api/breeds/list/all')
-      .then((response) => {
-        this.setState({
-          breeds: response.data.message,
+      .then((res) => {
+        this.setState((state) => {
+          const breeds = Object.keys(res.data.message);
+          return { breeds };
         });
-        console.log(response);
       })
       .catch(err => {
         console.log(err);
       });
   }
 
-
   render() {
+    console.log(this.state.breeds);
     return (
       <View style={styles.container}>
-        {this.state.breeds.keys.map((breed) => {
-          return(
-            <Text>{breed}</Text>
-          );
-        })}
+        <Text>Dog List</Text>
+          {this.state.breeds.map((breed) => {
+            return(
+              <Text>{breed}</Text>
+            );
+          })}
+        <Text>end of dog list</Text>
       </View>
     );
   }
