b218bbddb67741a71a8bf6dea9efe39bfabbe7a4
Christy Crites <cacrites2007@gmail.com>
2018-01-12 21:35:18 -0500

    refactored the stylesheet and cleaned up the code

--- dogbreedlist/App.js
+++ dogbreedlist/App.js
@@ -1,7 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View,TouchableHighlight } from 'react-native';
 import { StackNavigator } from 'react-navigation';
+
 import { BreedList, SubBreedList } from './Components';
+
+import { styles } from './stylesheet.js';
 
 //I tired to make the names as easy to follow as possible.  This definitely was a challenge! 
 //There's only so many ways you can say, "dog breed" and 'sub dog breed" before you just 
@@ -19,29 +22,73 @@ class Home extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: 'grey',
-  },
-  button: {
-    height: 50,
-    backgroundColor: 'white',
-    alignSelf: 'center',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  buttonText: {
-    fontSize: 22
-  }
-});
-
 const Routes = StackNavigator({
   Home: {screen: Home},
   BreedList: {screen: BreedList},
   SubBreedList: {screen: SubBreedList}
 });
 
-export default Routes; 
+export default Routes;
+
+//expo output just incase this doesn't work. 
+
+// 5:26:52 PM
+// this.props Object {
+//   "navigation": Object {
+//     "dispatch": [Function anonymous],
+//     "goBack": [Function goBack],
+//     "navigate": [Function navigate],
+//     "setParams": [Function setParams],
+//     "state": Object {
+//       "key": "id-1515795997280-2",
+//       "params": Object {
+//         "subBreeds": Array [
+//           "boston",
+//           "french",
+//         ],
+//       },
+//       "routeName": "SubBreedList",
+//     },
+//   },
+//   "screenProps": undefined,
+// }
+// 5:26:52 PM
+// this.props.navigation.state Object {
+//   "key": "id-1515795997280-2",
+//   "params": Object {
+//     "subBreeds": Array [
+//       "boston",
+//       "french",
+//     ],
+//   },
+//   "routeName": "SubBreedList",
+// }
+// 5:26:52 PM
+// this.props.navigation.state.params Object {
+//   "subBreeds": Array [
+//     "boston",
+//     "french",
+//   ],
+// }
+// 5:26:52 PM
+// this.props.navigation.state.params.subBreeds Array [
+//   "boston",
+//   "french",
+// ]
+// 5:26:52 PM
+// id undefined
+// 5:26:52 PM
+// breeds Array [
+//   Object {
+//     "breed": "boston",
+//     "id": 0,
+//   },
+//   Object {
+//     "breed": "french",
+//     "id": 1,
+//   },
+// ]
+// 5:26:52 PM
+// state Object {
+//   "breeds": Array [],
+// }
--- dogbreedlist/Components/breedList.js
+++ dogbreedlist/Components/breedList.js
@@ -19,57 +19,25 @@ export default class BreedList extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <View style={styles.breedListContainer}>
         <Text>{this.state.error ? this.state.error : null}</Text>
-        <Text style={styles.List}> Dog Breed List - Touch The Pink Names to See the SubBreed </Text>
+        <Text style={styles.breedList}> Dog Breed List - Touch The Pink Names to See the SubBreed </Text>
         <FlatList 
           keyExtractor={item => item[0]}
           data={this.state.breeds}
           renderItem={({ item }) => {
             return (
-              <View style={styles.List}>
+              <View style={styles.breedList}>
                 {item[1].length ?
                   <TouchableHighlight onPress={() => {
                     this.props.navigation.navigate('SubBreedList', {subBreeds: item[1]})}} >
-                    <Text style={styles.Link}>
-                    {/* if you make this {item} it will render the subBreeds as well - found that out the hard way*/}
-                      {item[0]} 
-                    </Text>
-                  </TouchableHighlight> : 
-                  <Text style={styles.ListItems}>
-                    {item}
-                  </Text>
+                    <Text style={styles.breedListLink}> {item[0]} </Text>
+                    {/* if you make this {item} it will render the subBreeds as well - found that out the hard way*/} 
+                  </TouchableHighlight> : <Text style={styles.breedListItems}> {item} </Text>
                 }
               </View>
-            );
-          }}
-        />
+          )}}/>
       </View>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'black',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  List: {
-    height: 50,
-    width: 150,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  Link: {
-    color: 'pink',
-    fontSize: 22,
-  },
-  ListItems: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: 'white',
-    fontSize: 22
-  }
-});
