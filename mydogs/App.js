f4617443fc4c27a99201980726519cc7d0fe8987
Sam Kim <sams404link@gmail.com>
2018-01-15 16:29:31 -0800

    sprint updated

--- mydogs/App.js
+++ mydogs/App.js
@@ -3,34 +3,58 @@ import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
 import axios from 'axios';
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-    doglist : {},
-    error: ''
-    }
+  state = {
+    doglist : []
   }
-  componentDidMount() {
+  
+  componentWillMount() {
    axios.get('https://dog.ceo/api/breeds/list/all')
     .then(res => {
-      this.setState({
-        doglist : res.data.message
-      })
+      this.setState(prevState => {
+        let { doglist } = prevState;
+        const data = [];
+        let tempDogList = res.data.message;
+        let index = 0;
+        for(let dog in tempDogList) {
+          if(tempDogList.hasOwnProperty(dog)) {
+            data.push({breed: dog, key: index} )
+            index += 1;
+          }
+        }
+        return { doglist: data }
+      });
+    
     })
     .catch(err => {
       console.log(err)
     })
   }
+  renderDogList() {   
+    console.log(this.state.doglist)
+    return(
+      <View>
+        <FlatList
+          style={styles.list}
+          data={this.state.doglist}
+          renderItem={({item, index}) => {
+            return (
+              <View key = {item.key}>
+                <Text> {item.breed} </Text>
+              </View>
+            )
+          }}
+          keyExtractor={(item, index) => index}
+        />
+      </View>
+    )
+  }
   render() {
     return (
-      
         <View style={styles.container}>
-          <Text>DogList </Text>
-          <FlatList 
-            data={Object.keys(this.state.doglist)}
-            renderItem={({item, index}) => {
-              <View key={item}>{item}</View>
-            }}></FlatList>      
+          <View>
+          <Text style={styles.header}>DogList</Text>
+          </View>
+          {this.renderDogList()}
         </View>
     );
   }
@@ -43,6 +67,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header: {
+    marginTop: 100,
+    marginBottom:10,
+    fontSize: 25
+  },
+  list: {
+    marginBottom: 100
+  }
 });
 /*
 {this.state.doglist.map((dog, index) => {
