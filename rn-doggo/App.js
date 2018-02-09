e43673bd391a1d8f1fe3765513221ee96ca4b2d6
Jason Metten <metten@gmail.com>
2018-02-09 13:02:21 -0600

    Out of time, whole challenge spent troubleshooting

---
+++ rn-doggo/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Text, View, Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+import styles from './Styles';
+
+import Dogs from './Dogs';
+
+class Home extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<View style={styles.header} />
+				<Text style={styles.textTitle}>Welcome to my Crazy App of Dog Breeds</Text>
+				<Button
+					title="Dog Breeds"
+					onPress={() => {
+						this.props.navigation.navigate('Dogs');
+					}}
+				/>
+			</View>
+		);
+	}
+}
+
+const Routes = StackNavigator({
+	Home: { screen: Home },
+	Dogs: { screen: Dogs }
+});
+
+export default Routes;
