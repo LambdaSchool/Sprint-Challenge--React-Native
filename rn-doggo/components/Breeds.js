8d03d75e05857bb39ef60c8ed6517036242a7589
atiffany <abby.tiffany@gmail.com>
2018-02-09 10:29:04 -0800

    flat list not working

--- rn-doggo/components/Breeds.js
+++ rn-doggo/components/Breeds.js
@@ -1,11 +1,35 @@
 import React from 'react';
-import { StyleSheet, View, Text, Button, TouchableOpacity } from 'react-native';
+import { StyleSheet, View, Text, Button, TouchableOpacity, FlatList } from 'react-native';
+import Axios from 'axios';
 
 export default class Breeds extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            breeds: [],
+        };
+    }
+
+    componentWillMount() {
+        Axios
+            .get('https://dog.ceo/api/breeds/list/all')
+            .then(res => {
+                this.setState({ breeds: res.data.message });
+                console.log(this.state.breeds);
+            })
+            .catch(error => {
+                console.log('There was an error fetching data: ', error);
+            });
+    };
+
     render() {
         return (
             <View style={container}>
                 <Text>Doggo Breeds Go Here</Text>
+                <FlatList 
+                    data={this.state.breeds}
+                    renderItem={({ item }) => <Text>{item}</Text>}
+                />
             </View>
         );
     }
@@ -20,3 +44,16 @@ const styles = StyleSheet.create({
     },
   });
   const { container } = styles;
+
+  /*
+        <FlatList
+                data = {this.state.breeds}
+                renderItem = {({ breed, index }) => {
+                    return(
+                        <View key={breed}>
+                            <Text>{breed[0]}</Text>
+                        </View>
+                    );
+                }}
+                />
+                */
