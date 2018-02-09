051243e426b6d6e99b498631e4b20fca6379458a
Jamie Domingo <jaybmw@aol.com>
2018-02-09 11:13:58 -0800

    added axios

---
+++ rn-doggo/Components/Breeds.js
@@ -0,0 +1,70 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { View, Text, StyleSheet, FlatList, List } from 'react-native';
+const URL = 'https://dog.ceo/api/breeds/list/all';
+
+class Breeds extends Component {
+    constructor() {
+        super();
+        this.state = {
+            data: [],
+            error: null,
+        };
+    }
+
+
+    showBreeds = () => {
+        axios.get(`${URL}`, )
+            .then(response => {
+                const { message } = response.data;
+                this.setState({ data: message });
+                console.log('this.state.data', this.state.data);
+            })
+            .catch(error => {
+                console.log(error);
+                this.setState({
+                    error: 'Error retrieving data',
+                });
+                setTimeout(() => {
+                    this.setState({ error: null });
+                }, 3000);
+            });
+
+    };
+
+    keyExtractor = (item, index) => item.id;
+
+    renderItem = ({ item }) => (
+        <MyListItem
+            id={item.id}
+            onPressItem={this._onPressItem}
+            selected={!!this.state.selected.get(item.id)}
+            title={item.title}
+        />
+    );
+
+    render() {
+        return (
+            <View style={{ padding: 10 }}>
+                <Text style={heading}>Dog Breeds</Text>
+                <FlatList data={this.state.deals} style={{ flex: 1, padding: 10 }} extraData={this.state} keyExtractor={this.keyExtractor} renderItem={this.renderItem} />
+
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    heading: {
+        margin: 30,
+        fontSize: 20
+    }
+})
+
+const { heading } = styles;
+
+export default Breeds;
+
+
+
+
