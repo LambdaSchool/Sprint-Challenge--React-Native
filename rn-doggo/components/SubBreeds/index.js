d17b89c5bfb3a2e1c0e76c8d2678c7899d995344
Moises Dobarganes <designerexpert@gmail.com>
2018-02-09 14:48:37 -0500

    Sub Breeds Functional and Showing, More implementation needed

--- rn-doggo/components/SubBreeds/index.js
+++ rn-doggo/components/SubBreeds/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, ScrollView } from 'react-native';
 import { StackNavigator } from 'react-navigation';
 import axios from 'axios';
 import Styles from '../Styles';
@@ -10,29 +10,74 @@ export default class SubBreeds extends Component {
         super(props);
         this.state = {
             subBreed: '',
+            subBreedList: ['This Breed has no known sub breeds']
         }
     }
-    componentWillMount() {
-        let subBreed = this.props.navigation.state.params.subBreed
-        this.setState({ subBreed })
+    keyExtractor = (item, index) => {
+        return (`${item}${index}`);
     }
-    capitalizeBreedName() {
-        let nameCopy = this.state.subBreed;
-        nameCopyArr = nameCopy.split('')
+
+    componentWillMount() {
+        let subBreed = this.props.navigation.state.params.subBreed;
+        console.log('subBreed is: ', subBreed)
+        axios.get(`https://dog.ceo/api/breed/${subBreed}/list`)
+            .then(res => {
+                let subBreedUppercase = this.capitalizeBreedName(subBreed);
+                let subBreedList = res.data.message;
+                if (subBreedList.length > 0) {
+                    this.setState({ subBreed: subBreedUppercase, subBreedList })
+                } else {
+                    this.setState({ subBreed: subBreedUppercase })
+                }
+
+            })
+            .catch(err => {
+                console.log('Error Getting SubBreeds List')
+            })
+    }
+    capitalizeBreedName(name) {
+        let nameCopyArr = name.split('');
         nameCopyArr[0] = nameCopyArr[0].toUpperCase();
         return nameCopyArr.join('');
     }
-
-    componentWillUnmount() {
-
+    handleListResult() {
+        let list = this.state.subBreedList;
+        if (list[0] != 'This Breed has no known sub breeds') {
+            return (
+                <ScrollView contentContainerStyle={Styles.listWrapper}>
+                    <FlatList
+                        keyExtractor={this.keyExtractor}
+                        data={this.state.subBreedList}
+                        renderItem={({ item }) => {
+                            return (
+                                <Text
+                                    style={Styles.listText}
+                                    name={item}
+                                >
+                                    {item}
+                                </Text>
+                            );
+                        }}
+                    />
+                </ScrollView>
+            );
+        } else {
+            return (
+                <Text style={Styles.instructions}>{list[0]}</Text>
+            );
+        }
     }
+
     render() {
         console.log(this.state)
         return (
             <View style={Styles.container}>
                 <Text style={Styles.pageHeader}>
-                    {`${this.capitalizeBreedName()} SubBreeds`}
+                    {`${this.state.subBreed} SubBreeds`}
                 </Text>
+
+                {this.handleListResult()}
+
             </View>
         );
     }
