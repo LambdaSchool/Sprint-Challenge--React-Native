44fee3ebff00b593ef15c06294a08a629cc48f45
FJF616 <franzjfernando@hotmail.com>
2018-02-09 11:23:51 -0800

    prints list to console but not device

--- rn-doggo/App.js
+++ rn-doggo/App.js
@@ -1,70 +1,68 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList, AsyncStorage, TouchableHighlight } from 'react-native';
 import axios from 'axios';
 
 export default class App extends React.Component {
-  constructor() {
-    super();
-    state = {
-      name: '',
-      breeds : []
+    constructor() {
+        super();
+        this.state = {
+        message: [],
+        text:'',
+        error: null
+    }
+};
+    onPressButtonGET = () => {
+        fetch("https://dog.ceo/api/breeds/list", {method: "GET"})
+        .then((response) => response.json())
+        .then((responseData) => {
+            const {message} = responseData;
+                this.setState(message.data);
+                console.log(responseData);
+        })
+        .done();
     };
-  }
-  componentDidMount = () => {
-    axios.get('https://dog.ceo/api/breeds/list/', {
-      })
-      .then(res => {
-        res.json();
-      })
-      .then(responseJson => {
-        console.log(responseJson); 
-        this.setState({
-          name: responseJson
-        });
-      })
-      .catch(err => {
-        console.error(err);
-        this.setState({error: 'Error Fetching List'});
-      })
-      .catch(err => {
-        console.log(err);
-      });
+    render() {
+        return (
+            <View style={container}>
+                <TouchableHighlight onPress={() => this.onPressButtonGET()} style={button}>
+                    <Text>GET</Text>
+                </TouchableHighlight>
+                <FlatList
+                    data={this.state.message}
+                    renderItem={({ item, key }) => {
+                    return (
+                      <View key={item.key}>
+                        <Text style={listItem} >
+                        {item}
+                        </Text>
+                        </View>  
+                        );
+                     }}
+                  />
+            </View>
+        );
     }
-  render() {
-    return (
-        <View style={container}>
-          <FlatList
-          data={this.state.breeds}
-          renderItem={({ item, key }) => {
-          return (
-            <View key={item.key}>
-              <Text style={styles.listItem} >
-              {item.name}
-              </Text>
-            </View>  
-            );
-          }}
-          />
-        </View>
-      )
-    }
-  }
-    
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  textFont: {
-    fontSize: 28
-  },
-  listItem: {
-    paddingTop: 2,
-    paddingBottom: 2,
-    fontSize: 18
-  },
+}
+
+var styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+    button: {
+        backgroundColor: '#eeeeee',
+        padding: 10,
+        marginRight: 5,
+        marginLeft: 5,
+    },
+    listItem: {
+        paddingTop: 2,
+        paddingBottom: 2,
+        fontSize: 18
+      },
 });
 
-const{ container, textFont, listItem } = styles;
+const { listItem, button, container } = styles;
