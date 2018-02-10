dc6bf5160d94c652bd7eb52a3e5dee0e2aade382
Aaron Wilder <element1028@gmail.com>
2018-02-09 16:17:14 -0800

    got the list to show up

--- rn-doggo/App.js
+++ rn-doggo/App.js
@@ -1,83 +1,37 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button, FlatList, ScrollView } from 'react-native';
 import axios from 'axios';
-const renderIf = (condition, content) => {
-  if (condition) {
-    return content
-  } else {
-    return null
-  }
-}
+
 export default class App extends React.Component {
   constructor() {
     super() 
       this.state = {
-        listofDogs: []
+        Dogs: []
       }
-      this.getAll = this.getAll.bind(this)
-      this.whatsInState = this.whatsInState.bind(this)
-  }
-  getAll() {
-  axios.get('https://dog.ceo/api/breeds/list/all')
-    .then((response) => {
-      const {data} = response;
-      const Dogs = Object.keys(data.message)
-      console.log('This is from Response', Dogs)
-      this.setstate =({
-        listofDogs: this.state.listofDogs = Dogs 
-      })
-    })
-    .catch((error) => {
-      console.log(error)
-    })
-}
-  componentWillUpdate() {
-    if(this.state.listofDogs.length !== 0 ){
-      return true
-     this.listOfStuff
-    }
-  }
-  listOfStuff() {
-    const { listofDogs } = this.state
-    return (
-      <ScrollView>
-            <FlatList data={listofDogs}
-                      keyExtractor={(item, index) => item + index}
-                      renderItem={({item, index})=> {
-                        return <Text>{item}</Text>
-                      }}
-          />
-         </ScrollView>
-    )
-  }
-  
-  whatsInState() {
-    console.log('This is state',this.state.listofDogs)
   }
 
+  componentWillMount() {
+    axios.get('https://dog.ceo/api/breeds/list/all')
+            .then(res => {
+                console.log('Dogs in the house');
+                const dogs = Object.keys(res.data.message);
+                this.setState({ dogs })
+            })
+            .catch(err => {
+                console.log(err, 'no dogs in the house');
+            })
+  }
   render() {
-    const { listofDogs } = this.state
-    console.log(listofDogs)
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Button title="all" onPress={this.getAll}/>
-        <Button title="console.log state" onPress={this.whatsInState}/>
-         <View>
-         {renderIf(this.state.listofDogs.length !== 0,
-            <ScrollView>
-            <FlatList data={listofDogs}
+        <ScrollView>
+          <FlatList data={this.state.dogs}
                       keyExtractor={(item, index) => item + index}
                       renderItem={({item, index})=> {
                         return <Text>{item}</Text>
                       }}
           />
-         </ScrollView>
-            
-            )}{this.listOfStuff()}
-        </View>
+        </ScrollView>
       </View>
     );
   }
